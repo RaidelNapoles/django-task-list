21d99d68ddcbff5e9158356619dd7e7f03a6ce42
Raidel <raidelnapoles95@gmail.com>
2024-05-04 11:51:33 -0400

    implementing basic model test

--- tasks_app/tests.py
+++ tasks_app/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Task
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user(username='admin', password='admin2020')
+        self.task = Task.objects.create(
+            title='Test Task',
+            description='Task created for testing purposes',
+            owner=self.test_user
+        )
+    
+    def test_object_name_is_title(self):
+        task = Task.objects.get(id=1)
+        expected_object_name = f'{task.title}'
+        self.assertEqual(str(task), expected_object_name)
