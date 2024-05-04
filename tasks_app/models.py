457ee4f2a02d39ca9e03b526a89168bf4465a7de
Raidel <raidelnapoles95@gmail.com>
2024-05-04 01:14:37 -0400

    first commit (created basic model and project run)

---
+++ tasks_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Task(models.Model):
+    class State(models.TextChoices):
+        PENDING = 'PEN', 'PENDING'
+        IN_PROGRESS = 'IPR', 'IN_PROGRESS'
+        COMPLETED = 'CPD', 'COMPLETED'
+        
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    state = models.CharField(max_length=3, choices=State.choices, default=State.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return self.title
