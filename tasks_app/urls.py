848fa20fc04be3ed83713d5e39517ba013b16978
Raidel <raidelnapoles95@gmail.com>
2024-05-04 04:34:36 -0400

    implementing endpoints and serializers

---
+++ tasks_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('task/', views.TaskList.as_view(), name='task-list'),
+    path('task/<int:pk>/', views.TaskDetail.as_view(), name='task-detail'),
+    path("", views.api_root),
+]
--- tasks_app/views.py
+++ tasks_app/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
-# Create your views here.
+
+class TaskList(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+
+class TaskDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+
+@api_view()
+def api_root(request, format=None):
+    return Response({
+        'task': reverse('task-list', request=request, format=format)
+    })
+
+
